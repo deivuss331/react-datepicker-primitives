906e6f44580f86a652926cbf99289f86c07f17f3
Dawid Zajac <zajdawid@protonmail.com>
2022-12-28 21:16:41 +0100

    feat: export primitives ⤴

---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export { default as PickerProvider } from './primitives/PickerProvider';
+export { default as PreviousRangeButton } from './primitives/PreviousRangeButton';
+export { default as NextRangeButton } from './primitives/NextRangeButton';
+export { default as RangeDateRenderer } from './primitives/RangeDateRenderer';
